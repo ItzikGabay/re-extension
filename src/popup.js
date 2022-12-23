cbd37a46a0260fbf45292677977bab3e2e6687af
Itzikgabay <itzikdevio@gmail.com>
2022-12-23 14:17:49 +0200

    `Popup` refactoring

--- src/popup.js
+++ src/popup.js
@@ -1,27 +1,34 @@
 'use strict';
 
 import './popup.css';
+import user from '../lib/userStorage';
+import { ERRORS } from '../lib/utils/errors';
+import { CONSTANTS } from '../lib/utils/constants';
 
 (function () {
-  function updateCounter({ type }) {
-    // Communicate with content script of
-    // active tab by sending a message
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      const tab = tabs[0];
+  document.addEventListener('DOMContentLoaded', async function () {
+    const addExtensionBtn = document.getElementById(
+      CONSTANTS.ADD_EXTENSION_BUTTON_ID
+    );
 
-      chrome.tabs.sendMessage(
-        tab.id,
-        {
-          type: 'COUNT',
-          payload: {
-            count: newCount,
-          },
-        },
-        (response) => {
-          console.log('Current count value passed to contentScript file');
-        }
-      );
+    addExtensionBtn.addEventListener('click', function (e) {
+      const inputValue = document.getElementById(
+        CONSTANTS.ADD_EXTENSION_INPUT_ID
+      ).value;
+      user.addExtension(inputValue);
     });
-  }
-  // document.addEventListener('DOMContentLoaded', restoreCounter);
+
+    const extensions = await user
+      .getAsync(CONSTANTS.EXTENSIONS_STORAGE_KEY)
+      .catch(() => console.log(ERRORS.NO_EXTENSION_FOUND));
+
+    document.querySelector('.list').innerHTML =
+      '<ul>' +
+      extensions
+        .map((extension) => {
+          return `<li>${extension}</li>`;
+        })
+        .join('\n') +
+      '</ul>';
+  });
 })();

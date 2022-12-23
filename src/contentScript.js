e2e65347bbb9fcfe24353a8dc169318de6b17aa2
Itzikgabay <itzikdevio@gmail.com>
2022-12-23 14:18:03 +0200

    `Content-script` refactoring

--- src/contentScript.js
+++ src/contentScript.js
@@ -1,32 +1,20 @@
 'use strict';
 
-import user from '../lib/userStorage';
-const userStorage = user.storage;
-
 const initializeUpdate = async () => {
-  try {
-    chrome.runtime.sendMessage({ type: 'UPDATE_EXTENSION' }, (response) => {
-      console.log('Response from UPDATE_EXTENSION:', response);
-      if (response.success) {
-        console.log('Extension updated!');
-        window.location.reload();
-      }
-      return true;
-    });
-  } catch (e) {
-    console.log(e);
-  }
+  console.log('[auto-refresher] initialize');
+
+  // try {
+  //   chrome.runtime.sendMessage({ type: 'UPDATE_EXTENSION' }, (response) => {
+  //     if (response.success) {
+  //       console.log('Extension updated successfully');
+  //       window.location.reload();
+  //     }
+  //
+  //     return true;
+  //   });
+  // } catch (e) {
+  //   console.log(e);
+  // }
 };
-
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  const { type } = request;
-
-  // Refresh page event
-  if (type === 'REFRESH_PAGE') {
-    console.log('REFRESH_PAGE');
-    sendResponse({});
-    return true;
-  }
-});
 
 initializeUpdate();
